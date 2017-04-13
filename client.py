5d2392fba243c6316a503ebfac05910156462cdd
Gabriel T <gabriel.tapuc@gmail.com>
2017-04-14 00:29:45 +1000

    Implemented too many tries block.

--- client.py
+++ client.py
@@ -14,24 +14,37 @@ global SERVER_IP, SERVER_PORT
 
 def start_chat():
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((SERVER_IP, SERVER_PORT))
+    try:
+        clientSocket.connect((SERVER_IP, SERVER_PORT))
 
-    while 1:
-        server_msg = clientSocket.recv(1024)
-        if server_msg == '':
-            continue
+        while 1:
+            server_msg = clientSocket.recv(1024)
+            if server_msg == '':
+                continue
 
-        server_msg = server_msg.split()
-        command = server_msg[0]
-        if command == '#prompt':
-            usr_input = raw_input('%s ' % server_msg[1])
-            clientSocket.send(usr_input)
-        elif command == '#info':
-            print '> %s' % ' '.join(server_msg[1:])
-        else:
-            print '> %s' % ' '.join(server_msg)
-            clientSocket.close()
-            break
+            server_msg = server_msg.split()
+            command = server_msg[0]
+            if command == '#prompt':
+                usr_input = raw_input('> %s ' % server_msg[1])
+                clientSocket.send(usr_input)
+            elif command == '#info':
+                print '> %s' % ' '.join(server_msg[1:])
+            elif command == '#terminate':
+                print '> %s ' % ' '.join(server_msg[1:])
+                clientSocket.close()
+                break
+            else:
+                # TODO figure out what to do when we don't recognize the command
+                print '> %s' % ' '.join(server_msg)
+                clientSocket.close()
+                break
+
+    except KeyboardInterrupt:
+        print 'KEYBOARD_INTERRUPT! Shutting down client gracefully'
+        clientSocket.close()
+    except error:
+        print 'Server is shut down. Terminating client'
+        clientSocket.close()
 
 if __name__ == '__main__':
     cmdArgs = sys.argv
--- server.py
+++ server.py
@@ -8,11 +8,16 @@ Server should the following in order:
 """
 
 import sys
+import datetime as time
 from socket import *
 
+# TODO find out how to get rid of global here
 global SERVER_PORT, BLOCK_DURATION, TIMEOUT
 credentials_filename = 'credentials.txt'
 login_history = {}
+# dictionary storing usernames of people blocked due of incorrect passwords
+# stored as 'username': 'date_when_blocked'
+blocked_login_attempts = {}
 
 # load credentials
 creds_file = open(credentials_filename, 'r')
@@ -20,49 +25,75 @@ creds_lines = creds_file.readlines()
 creds_lines = [line.translate(None, '\n') for line in creds_lines] # remove \n characters from credentials
 
 
-def prompt_credentials(sock):
+def prompt_credentials(sock, username):
     accepted = False
+    blocked = False
 
-    sock.send('#prompt Username:')
-    username = sock.recv(1024)
     sock.send('#prompt Password:')
     password = sock.recv(1024)
 
+    # check if blocked
+    if username in blocked_login_attempts:
+        time_now = time.datetime.now()
+        time_elapsed = time_now - blocked_login_attempts[username]
+        if time_elapsed.seconds > TIMEOUT:
+            del blocked_login_attempts[username]
+        else:
+            blocked = True
+
     # check credentials
-    creds = '%s %s' % (username, password)
-    if creds in creds_lines:
-        accepted = True
+    if not blocked:
+        creds = '%s %s' % (username, password)
+        if creds in creds_lines:
+            accepted = True
 
-    return accepted
+    return accepted, blocked
 
+def send_prompt(sock, msg):
+    sock.send('#prompt %s' % msg)
+    return
 
 def start_server():
     serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(('', SERVER_PORT))
-    serverSocket.listen(1)
-    print 'The intergalactic chat server is listening for connections'
+    try:
+        serverSocket.bind(('', SERVER_PORT))
+        serverSocket.listen(1)
+        print 'The intergalactic chat server is listening for connections'
 
-    while 1:
-        connectionSocket, addr = serverSocket.accept()
+        while 1:
+            connectionSocket, addr = serverSocket.accept()
 
-        # prompt for credentials
-        tries = 0
-        accepted = False
-        while tries < 3:
-            accepted = prompt_credentials(connectionSocket)
-            if accepted:
-                break
+            # prompt for credentials
+            tries = 0
+            accepted = False
+            blocked = False
+            send_prompt(connectionSocket, 'Username:')
+            username = connectionSocket.recv(1024)
+            while tries < 3:
+                accepted, blocked = prompt_credentials(connectionSocket, username)
+                if blocked:
+                    break
+                if accepted:
+                    break
+                else:
+                    if tries + 1 != 3:
+                        connectionSocket.send('#info Invalid Password. Please try again')
+                    tries += 1
+
+            if blocked:
+                connectionSocket.send('#terminate Your account is blocked due to multiple login failures. ' +
+                                      'Please try again later')
+                connectionSocket.close()
+            elif accepted:
+                connectionSocket.send('Welcome to the intergalactic chat service!')
+                connectionSocket.close()
             else:
-                if tries + 1 != 3:
-                    connectionSocket.send('#info Invalid Password. Please try again')
-                tries += 1
-
-        if accepted:
-            connectionSocket.send('Welcome to the intergalactic chat service!')
-            connectionSocket.close()
-        else:
-            connectionSocket.send('Invalid Password. Your account has been blocked. Please try again later')
-            connectionSocket.close()
+                blocked_login_attempts[username] = time.datetime.now()
+                connectionSocket.send('#terminate Invalid Password. Your account has been blocked. Please try again later')
+                connectionSocket.close()
+    except KeyboardInterrupt:
+        print 'Intergalactic chat server is shutting down gracefully'
+        serverSocket.close()
 
 if __name__ == '__main__':
     cmdArgs = sys.argv
@@ -75,10 +106,4 @@ if __name__ == '__main__':
         BLOCK_DURATION = cmdArgs[2]
         TIMEOUT = cmdArgs[3]
         start_server()
-
-
-
-
-
-
 
